ca40236c42618a1e5ef81f222d78a04e5912e91c
Rajat Saxena <rajatsxn94@gmail.com>
2020-03-21 10:47:27 -0700

    Add files via upload

--- ripple_analysis/detect_ripple.py
+++ ripple_analysis/detect_ripple.py
@@ -54,7 +54,17 @@ for filename in os.listdir(os.getcwd()):
         ripple_peak_idx = []
         ripple_peak_amp = []
         
-        # ripple peak detection
+        # data to hold the variables
+        ripple_durationv2 = []
+        ripple_start_timev2 = []
+        ripple_end_timev2 = []
+        ripple_start_idxv2 = []
+        ripple_end_idxv2 = []
+        ripple_peak_timev2 = []
+        ripple_peak_idxv2 = []
+        ripple_peak_ampv2 = []
+        
+        # ripple peak detection using ripple power threshold
         # find peaks using height as power thershold, min ripple distance = 200ms
         idx_peak = detect_peaks(ripple_power, mph=ripplePowerThresh, mpd=.1*fs)
         # iterate over each peak
@@ -92,12 +102,72 @@ for filename in os.listdir(os.getcwd()):
                 ripple_end_time.append(time[endidx])
                 ripple_peak_time.append(time[idx])
                 ripple_peak_amp.append(ripple_power[idx])
+                
+        # ripple peak detection using z-score threshold
+        # find peaks using height as power thershold, min ripple distance = 200ms
+        idx_peak2 = detect_peaks(filt_rip_env_zscore, mph=3.25, mpd=.1*fs)
+        # iterate over each peak
+        for idx in idx_peak2:
+            # nice trick: no point looking beyond +/- 300ms of the peak
+            # since the ripple cannot be longer than that
+            idx_max = idx + int(maxThreshTime*fs)
+            idx_min = idx - int(maxThreshTime*fs)
+            # boundary check
+            if idx_min<0:
+                idx_min = 0
+            # find the left and right falloff points for individual ripple
+            filt_rip_env_zscore_sub = filt_rip_env_zscore[idx_min:idx_max]
+            idx_falloff = np.where(filt_rip_env_zscore_sub<=0.15)[0]
+            idx_falloff += idx_min
+            # find the start and end index for individual ripple
+            _, startidx = mea.find_le(idx_falloff, idx)
+            _, endidx = mea.find_ge(idx_falloff, idx)
+            # accounting for some boundary conditions
+            if startidx is None:
+                th = (maxThreshTime + minThreshTime)//2
+                startidx = idx - int(th*fs)
+            if endidx is None:
+                endidx = 2*idx - startidx
+            # duration CHECK!
+            dur = time[endidx]-time[startidx]
+#            print(time[idx], time[endidx], time[startidx], dur, dur>=minThreshTime and dur<=maxThreshTime)
+            # add the ripple to saved data if it passes duration threshold
+            if dur>=minThreshTime and dur<=maxThreshTime:
+                ripple_durationv2.append(dur)
+                ripple_start_idxv2.append(startidx)
+                ripple_end_idxv2.append(endidx)
+                ripple_peak_idxv2.append(idx)
+                ripple_start_timev2.append(time[startidx])
+                ripple_end_timev2.append(time[endidx])
+                ripple_peak_timev2.append(time[idx])
+                ripple_peak_ampv2.append(filt_rip_env_zscore[idx])
         
         # plot the processed data
         fig, (ax1,ax2,ax3,ax4) = plt.subplots(nrows=4, ncols=1, sharex=True)
         ax1.plot(time, eeg_sig, c='k')
+        for ind_ in range(len(ripple_peak_time)):
+            rect = patches.Rectangle((ripple_start_time[ind_],np.nanmin(eeg_sig)),
+                                     ripple_end_time[ind_] - ripple_start_time[ind_],
+                                     np.nanmax(eeg_sig) - np.nanmin(eeg_sig),
+                                     linewidth=1,edgecolor='none',alpha=0.5,facecolor='y')
+            ax1.add_patch(rect)
+        ax1.set_ylabel('Raw signal', fontsize=16)
         ax2.plot(time, filt_rip_sig, c='r')
+        for ind_ in range(len(ripple_peak_time)):
+            rect = patches.Rectangle((ripple_start_time[ind_],np.nanmin(filt_rip_sig)),
+                                     ripple_end_time[ind_] - ripple_start_time[ind_],
+                                     np.nanmax(filt_rip_sig) - np.nanmin(filt_rip_sig),
+                                     linewidth=1,edgecolor='none',alpha=0.5,facecolor='y')
+            ax2.add_patch(rect)
+        ax2.set_ylabel('Filtered signal', fontsize=16)
         ax3.plot(time, filt_rip_env_zscore, c='m')
+        for ind_ in range(len(ripple_peak_timev2)):
+            rect = patches.Rectangle((ripple_start_timev2[ind_],np.nanmin(filt_rip_env_zscore)),
+                                     ripple_end_timev2[ind_] - ripple_start_timev2[ind_],
+                                     np.nanmax(filt_rip_env_zscore) - np.nanmin(filt_rip_env_zscore),
+                                     linewidth=1,edgecolor='none',alpha=0.5,facecolor='gray')
+            ax3.add_patch(rect)
+        ax3.set_ylabel('zscored filtered envelope', fontsize=16)
         ax4.plot(time, ripple_power, c='g')
         ax4.plot(time[ripple_peak_idx], ripple_power[ripple_peak_idx], 'b.')
         for ind_ in range(len(ripple_peak_time)):
@@ -105,4 +175,5 @@ for filename in os.listdir(os.getcwd()):
                                      linewidth=1,edgecolor='none',alpha=0.5,facecolor='y')
             ax4.add_patch(rect)
         ax4.set_xlabel("Time [s]", fontsize=16)
+        ax4.set_ylabel("Ripple power", fontsize=16)
         plt.show()
